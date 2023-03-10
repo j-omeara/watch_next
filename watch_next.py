ba28884be4da979d29f33a925d930e9c78534f44
j-omeara <127307095+j-omeara@users.noreply.github.com>
2023-03-10 07:41:07 +0000

    Update watch_next.py
    import spacy and assign nlp and variables

---
+++ watch_next.py
@@ -0,0 +1,6 @@
+# import spacy, assign nlp
+import spacy
+nlp = spacy.load('en_core_web_md')
+
+# assign variables
+target_desc =  "Will he save their world or destroy it? When the Hulk becomes too dangerous for the Earth, the Illuminati trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace. Unfortunately, Hulk land on the planet Sakaar where he is sold into slavery and trained as a gladiator."
